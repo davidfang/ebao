1efb89b1e430a9e153d6f49dc044096a74839b27
fdjiangwu <fdjiangwu@yeah.net>
2017-02-27 21:22:36 +0800

    格式调整

--- app/message/Questions.js
+++ app/message/Questions.js
@@ -10,6 +10,7 @@ export default class Questions extends Component {
     constructor(props) {
         super(props);
         let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        
         this.state = {
             dataSource: ds.cloneWithRows([
                 {
