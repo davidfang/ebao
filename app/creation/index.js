c69b3a867a9473f98c0d9cce996b828c87ed9f14
fdjiangwu <fdjiangwu@yeah.net>
2017-03-01 18:04:24 +0800

    格式调整

--- app/creation/index.js
+++ app/creation/index.js
@@ -14,6 +14,7 @@ export default class Home extends Component {
     constructor(props) {
         super(props);
         let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        
         this.state = {
             index: 0,
             routes: [
